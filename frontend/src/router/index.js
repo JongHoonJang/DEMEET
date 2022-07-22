e3732186b4e37ce2fa35a583083f1c701883f63b
장종훈 <jangjr98@naver.com>
2022-07-22 13:19:26 +0900

    [FE : FEAT] frontend produce

---
+++ frontend/src/router/index.js
@@ -0,0 +1,47 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import MainView from '@/views/main/MainView'
+import DetailView from '@/views/main/DetailView'
+import ConferenceView from '@/views/conference/ConferenceView'
+import ProfileView from '@/views/account/ProfileView'
+import LoginView from '@/views/account/LoginView'
+
+const routes = [
+  {
+    path: '/',
+    name: 'MainView',
+    component: MainView
+  },
+  {
+    path: '/project/:project_pk',
+    name: 'DetailView',
+    component: DetailView
+  },
+  {
+    path: '/conference',
+    name: 'ConferenceView',
+    component: ConferenceView
+  },
+  {
+    path: '/profile/:user_pk',
+    name: 'ProfileView',
+    component: ProfileView
+  },
+  {
+    path: '/login',
+    name: 'LoginView',
+    component: LoginView
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+// router 가드
+// 회원인테 맴버인경우
+// 회원인데 맴버가 아닌경우
+// 비회원인경우
+
+
+export default router
