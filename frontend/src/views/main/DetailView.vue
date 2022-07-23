ac0dc98692d8fe87fdd581d790ff88277c6b4b15
장종훈 <jangjr98@naver.com>
2022-07-23 17:56:15 +0900

    [FE : FEAT] MainHeader.vue, ProjectList.vue

--- frontend/src/views/main/DetailView.vue
+++ frontend/src/views/main/DetailView.vue
@@ -1,12 +1,14 @@
 <template>
-  <div>
-    
-  </div>
+    <MainNav />
+    <h1>DETAIL</h1>
 </template>
 
 <script>
+import MainNav from '@/views/main/MainNav'
 export default {
-
+  components: {
+    MainNav,
+  }
 }
 </script>
 
