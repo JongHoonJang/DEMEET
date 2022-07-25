bb86e14d251f9bf7876534252e18478c54dc415b
pc5401 <pc5401@naver.com>
2022-07-25 10:20:47 +0900

    [FEAT] S07P12B309-87 : ConferenceView.vue

--- frontend/src/views/conference/ConferenceView.vue
+++ frontend/src/views/conference/ConferenceView.vue
@@ -1,15 +1,69 @@
 <template>
-  <div>
-    
-  </div>
+  <!-- <nav>
+
+  </nav> -->
+  <main>
+    <section>
+      <article>
+        <!-- 영상, 드로잉 등 -->
+        <ConferenceVideo/>
+      </article>
+      <footer>
+        <!-- 동작 버튼 등,  -->
+      </footer>
+    </section>
+    <aside>
+      <!-- 참자가 목록, 채팅 -->
+      <ConferenceUsers/>
+      <ConferenceChat/>
+    </aside>
+  </main>
 </template>
 
 <script>
-export default {
+import ConferenceVideo from './ConferenceVideo'
+import ConferenceUsers from './ConferenceUsers'
+import ConferenceChat from './ConferenceChat'
 
+export default {
+  components: {
+    ConferenceVideo,
+    ConferenceUsers,
+    ConferenceChat,
+},
 }
 </script>
 
-<style>
+<style scoped>
+main{
+  display: flex;
+  margin: 0;
+  height: 100vh;
+
+}
+
+section {
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+  background-color: rgb(13, 19, 30);
+}
+
+article {
+  height: 90%;
+}
+
+footer {
+  background-color: rgb(21, 29, 42);
+  height: 10%;
+}
+
+  aside{
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    width: 20%;
+    background-color: #fff;
+  }
 
 </style>
