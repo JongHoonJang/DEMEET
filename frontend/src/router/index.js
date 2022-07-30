9410f8a8bccd8dbde78eed977f11320c95e60d7b
장종훈 <jangjr98@naver.com>
2022-07-30 13:46:44 +0900

    [FE:FEAT] router 가드 설정

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -42,11 +42,34 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
-
-// router 가드
-// 회원인테 맴버인경우
-// 회원인데 맴버가 아닌경우
-// 비회원인경우
-
+// 로우터 가드
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem('token')
+  // 토근이 존재할때 로그인화면으로 가려고하면 메인으로 이동
+  if(to.name === 'LoginView') {
+    if(token) {
+      next({ name:'MainView' })
+    }
+  }
+  // 메인화면에서 토큰이 존재하지 않으면 로그인화면으로 이동
+  else if (to.name === 'MainView') {
+    if(!token) {
+      next({ name:'LoginView' })
+    }
+  }
+  // 상세페이지에서 토큰이 존재하지 않으면 로그인화면으로 이동
+  else if (to.name === 'DetailView') {
+    if(!token) {
+      next({ name:'LoginView' })
+    }
+  }
+  // 프로필화면에서 토큰이 존재하지 않으면 로그인화면으로 이동
+  else if (to.name === 'ProfileView') {
+    if(!token) {
+      next({ name:'LoginView' })
+    }
+  }
+  next()
+})
 
 export default router
