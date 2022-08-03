1b73021a7f631a242ea169551729b828d978356b
Wondae-code <noin196@naver.com>
2022-08-03 17:28:33 +0900

    [FEAT] S07P12B309-256 : experimenting amazon s3 image hosting service

---
+++ backend/src/main/java/com/ssafy/api/controller/AwsS3Controller.java
@@ -0,0 +1,11 @@
+package com.ssafy.api.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/S3")
+@RequiredArgsConstructor
+public class AwsS3Controller {
+}
