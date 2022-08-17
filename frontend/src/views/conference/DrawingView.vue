b27214245d57b5849f42dcea57e1c9fb6ed880a6
김대원 <noin1996@naver.com>
2022-08-18 00:54:10 +0900

    [FE:FIX] yorkie connect update

--- frontend/src/views/conference/DrawingView.vue
+++ frontend/src/views/conference/DrawingView.vue
@@ -43,7 +43,7 @@ export default {
       })
       fabric.Object.prototype.transparentCorners = true
       // yorkie Client 설정
-      const client = new yorkie.Client('http://localhost:8082', {
+      const client = new yorkie.Client('http://172.19.0.3:8082', {
         syncLoopDuration: 0,
         reconnectStreamDelay: 1000
       })
